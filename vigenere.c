6ff53e6a1834d18223139e151c5ba3311b2ccc00
greyspectrum <greyspectrum@users.noreply.github.com>
2016-07-01 13:25:52 +0000

    Create vigenere.c

---
+++ vigenere.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+ 
+int main(void)
+{
+    printf("Enter message for encryption: \n");
+    char p[50];
+    fgets(p, 50, stdin);
+    printf("Select keyword  (no caps): \n");
+    char key[15];
+    fgets(key, 15, stdin);
+    int i, n, j;
+    for(i = 0, j = 0, n = strlen(p); i < n; i++, j++)
+        {
+            if(p[i] <= 'a' && p[i] >= 'z')
+                printf("%c", (((p[i] - 'a') + (key[j] - 'a' ) %26) + 'a'));
+            if(p[i] <= 'a' && p[i] >= 'z')
+                printf("%c", (((p[i] - 'a') + (key[j] - 'a' ) %26) + 'a'));
+            else if(p[i] >= 'a' && p[i] <= 'z')
+                printf("%c", (((p[i] - 'a') + (key[j] - 'a') %26) + 'a'));
+            else if(p[i] >= 'a' && p[i] <= 'z')
+                printf("%c", (((p[i] - 'a') + (key[j] - 'a') %26) + 'a'));
+            else
+                printf("%c", p[i]);
+        }
+    printf("\n");
+    return 0;
+}
